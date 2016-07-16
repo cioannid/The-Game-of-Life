f4f7013f7b5430a9484e476cadd6bab33f1b8aa8
Christos@OfficePC <c-ioankoz@hotmail.com>
2016-07-16 21:07:12 +0300

    Added the functionality that computes the next state of the cell table

--- code.js
+++ code.js
@@ -34,6 +34,100 @@ function resetStateArrays() {
 	}
 }
 
+function computeNextGeneration() {
+	for (var row = 0; row < rows; row++) {
+		for (var col = 0; col < cols; col++) {
+			var numberOfNeighbors = getNumberOfNeighbors(row, col);
+			nextStateArray[row][col] = getNextStateForCell(currentStateArray[row][col], numberOfNeighbors);
+		}
+	}
+}
+
+function getNextStateForCell(currentState, numberOfNeighbors) {
+	if (currentState === 1) {
+		// currently live cell
+		if (numberOfNeighbors < 2 || numberOfNeighbors > 3) {
+			// this cell dies
+			return 0;
+		} else {
+			// this cell has either 2 or 3 live neighbors, so it survives
+			return 1;
+		}
+	} else {
+		// currently dead cell
+		if (numberOfNeighbors === 3) {
+			// this cell revives, as if by reproduction
+			return 1;
+		} else {
+			// the dead cell remains as is
+			return 0;
+		}
+	}
+}
+
+function getNumberOfNeighbors(row, col) {
+	if (isCellOnBorder(row, col)) {
+		return getNumberOfNeighborsForBorderCell(row, col);
+	} else {
+		return getNumberOfNeighborsForInteriorCell(row, col);
+	}
+}
+
+function getNumberOfNeighborsForInteriorCell(row, col) {
+	return (currentStateArray[row][col - 1] 
+		+ currentStateArray[row - 1][col - 1] + currentStateArray[row - 1][col] + currentStateArray[row - 1][col + 1] 
+		+ currentStateArray[row][col + 1]
+		+ currentStateArray[row + 1][col - 1] + currentStateArray[row + 1][col] + currentStateArray[row + 1][col + 1]);
+}
+
+function isCellOnBorder(row, col) {
+	 return (row === 0 || row === rows - 1 || col === 0 || col === cols - 1);
+}
+
+function getNumberOfNeighborsForBorderCell(row, col) {
+	if (isCornerCell(row, col)) {
+		return getCornerCellNumberOfNeighbors(row, col);
+	} else {
+		return getEdgeCellNumberOfNeighbors(row, col);
+	}
+}
+
+function isCornerCell(row, col) {
+	return ((row === 0 && col === 0) || (row === 0 && col === cols - 1) || (row === rows - 1 && col === 0) || (row === rows - 1 && col === cols - 1));
+}
+
+function getCornerCellNumberOfNeighbors(row, col) {
+	if (row === 0 && col === 0) {
+		// top left corner
+		return (currentStateArray[row][col + 1] + currentStateArray[row + 1][col] + currentStateArray[row + 1][col + 1]);
+	} else if (row === 0 && col === cols - 1) {
+		// top right corner
+		return currentStateArray[row][col - 1] + currentStateArray[row + 1][col] + currentStateArray[row + 1][col - 1];
+	} else if (row === rows - 1 && col === 0) {
+		// bottom left corner
+		return (currentStateArray[row][col + 1] + currentStateArray[row - 1][col] + currentStateArray[row - 1][col + 1]);
+	} else if (row === rows - 1 && col === cols - 1) {
+		// bottom right corner
+		return (currentStateArray[row][col - 1] + currentStateArray[row - 1][col] + currentStateArray[row - 1][col - 1]);
+	}
+}
+
+function getEdgeCellNumberOfNeighbors(row, col) {
+	if (row === 0) {
+		// north border
+		return (currentStateArray[row][col - 1] + currentStateArray[row + 1][col - 1] + currentStateArray[row + 1][col] + currentStateArray[row + 1][col + 1] + currentStateArray[row][col + 1]);
+	} else if (row === rows - 1) {
+		// south border
+		return (currentStateArray[row][col - 1] + currentStateArray[row - 1][col - 1] + currentStateArray[row - 1][col] + currentStateArray[row - 1][col + 1] + currentStateArray[row][col + 1]);
+	} else if (col === 0) {
+		// west border
+		return (currentStateArray[row - 1][col] + currentStateArray[row - 1][col + 1] + currentStateArray[row][col + 1] + currentStateArray[row + 1][col + 1] + currentStateArray[row + 1][col]);
+	} else if (col === cols - 1) {
+		// east border
+		return (currentStateArray[row - 1][col] + currentStateArray[row - 1][col - 1] + currentStateArray[row][col - 1] + currentStateArray[row + 1][col - 1] + currentStateArray[row + 1][col]);
+	}
+}
+
 // lay out the board
 function createTable() {
     var gridContainer = document.getElementById("gridContainer");
@@ -105,6 +199,7 @@ function clearButtonHandler() {
 // run the life game
 function play() {
     console.log("Play the game");
+    computeNextGeneration();
 }
 
 // start everything
